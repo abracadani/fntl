10e13ea8c209671511fd6deaaa164db25af26abe
Daniela Hidalgo <danihidalgos@gmail.com>
2019-01-28 18:35:28 -0300

    cont

--- fntl.py
+++ fntl.py
@@ -1,7 +1,21 @@
+class Portfolio():
+  def __init__(self, stocks=[]):
+    self.stocks = stocks
 
+  def Profit(start, end):
+    start_val = sum([x for x in stocks])
+    return start_val
 
-# class Stock():
-#   def __init__(self, ):
+class Stock():
+  def __init__(self, name, date, price):
+    self.name = name
+    self.date = date
+    self.price = price
+
+  def Price():
+    return self.price
+
+p = Portfolio()
 
 # Fake Stocks
 def fake_stock():
@@ -11,7 +25,7 @@ def fake_stock():
   price = randint(100,99999)/100
   return (name,price)
 
-def fake_portfolio(n=10,start='20181225',end='20181231'):
+def fake_portfolio(n=10,start='20170101',end='20181231'):
   from datetime import date, datetime
   from workdays import workday
   import numpy as np
@@ -19,13 +33,13 @@ def fake_portfolio(n=10,start='20181225',end='20181231'):
 
   stocks = [fake_stock() for i in range (0, n)]
   dt = datetime.strptime(start, '%Y%m%d')
+  result = dict()
   while dt <= datetime.strptime(end, '%Y%m%d'):
     price = [(value[0],round(list(np.random.normal(value[1],value[1]/randint(10,100),1))[0],2)) for key,value in enumerate(stocks)]
-    print(price)
     dt = workday(dt,1)
-    print(dt.strftime('%Y%m%d'))
+    result[dt.strftime('%Y%m%d')] = price
+  return result
 
-  # for i in range(0,stocks):
-  #   print(i)
-
-fake_portfolio()
+port = fake_portfolio()
+for k,v in enumerate(port):
+  print(v)
